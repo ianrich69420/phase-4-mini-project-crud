dc9c63ca29e56d49f7c3df6452eb62b066b79d77
Ian Richard <ian.orieko@student.moringaschool.com>
2023-03-25 03:35:58 +0000

    Completed assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,48 @@
+class SpicesController < ApplicationController
+    wrap_parameters format: []
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
+      # GET /spices
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    # POST /spices
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    # PATCH /spices/:id
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice, status: :accepted
+    end
+
+    # DELETE /spices/:id
+    def destroy
+        spice = find_spice
+        spice.destroy
+        render json: {}, status: :no_content
+    end
+
+    private
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Spice not found" }, status: :not_found
+    end
+
+end
